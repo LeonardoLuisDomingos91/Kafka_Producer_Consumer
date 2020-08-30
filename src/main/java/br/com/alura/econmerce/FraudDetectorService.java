18c1af1a337ce71a8053b35e4e9c5e0fdd4eaebe
Leonardo Luis Domingos <you@example.com>
2020-08-30 14:03:02 -0300

    Criando deserializer com Gson

--- src/main/java/br/com/alura/econmerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/econmerce/FraudDetectorService.java
@@ -2,18 +2,22 @@ package br.com.alura.econmerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+
 public class FraudDetectorService {
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
+        try(var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER",
-                fraudService::parse)){
+                fraudService::parse,
+                Order.class,
+                new HashMap<>())){
 
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record){
+    private void parse(ConsumerRecord<String, Order> record){
         System.out.println("--------------------------------------------");
         System.out.println("Processando nova ordem, checando se é fraude");
         System.out.println("KEY: "+record.key());
--- src/main/java/br/com/alura/econmerce/LogService.java
+++ src/main/java/br/com/alura/econmerce/LogService.java
@@ -1,34 +1,33 @@
 package br.com.alura.econmerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-
-import java.time.Duration;
+import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
 public class LogService {
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
-        while (true) {
-            var records = consumer.poll(Duration.ofMillis(100)); // perguntando se tem registro nesse tempo determinado e pegando os registro retornados
-            if (!records.isEmpty()) {
-//                System.out.println("Não encontrei" + records.count() + " regristo");
-                for (var record : records) {
-                    System.out.println("--------------------------------------------");
-                    System.out.println("LOG " + record.topic());
-                    System.out.println("KEY: "+record.key());
-                    System.out.println("Value: "+record.value());
-                    System.out.println("partition: "+record.partition());
-                    System.out.println("offset: "+ record.offset());
-                }
-            }
+        var logService = new LogService();
+        try(var service = new KafkaService(LogService.class.getSimpleName(),
+                Pattern.compile("ECOMMERCE.*"),
+                logService::parse,
+                String.class,
+                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
+            service.run();
         }
     }
 
+    private void parse(ConsumerRecord<String, String> record){
+            System.out.println("--------------------------------------------");
+            System.out.println("LOG " + record.topic());
+            System.out.println("KEY: "+record.key());
+            System.out.println("Value: "+record.value());
+            System.out.println("partition: "+record.partition());
+            System.out.println("offset: "+ record.offset());
+    }
 
     private static Properties properties() {
         var properties = new Properties(); // criando propriedade para retornar no consumidar
